e9484982420e4db163dd027cce23550f551ccd2b
Anna Wolford <17awolford@gmail.com>
2023-10-09 13:48:18 -0600

    added logistic regression

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -11,6 +11,11 @@ amazonTest <- vroom("./test.csv")
 table(amazonTrain$RESOURCE)
 table(amazonTrain$ROLE_ROLLUP_1, amazonTrain$ROLE_ROLLUP_2)
 # you should have 112 columns 
+amazonTrain <- amazonTrain %>%
+  mutate(ACTION = as.factor(ACTION))
+
+my_mod <- logistic_reg() %>%
+  set_engine("glm")
 
 my_recipe <- recipe(ACTION~., data=amazonTrain) %>%
   step_mutate_at(all_numeric_predictors(), fn=factor) %>%
@@ -18,4 +23,29 @@ my_recipe <- recipe(ACTION~., data=amazonTrain) %>%
   step_dummy(all_nominal_predictors())
 
 prep <- prep(my_recipe)
-baked <- bake(prep, new_data=amazonTrain)
+baked <- bake(prep, new_data=amazonTest)
+
+amazon_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data=amazonTrain)
+
+amazon_predictions <- predict(amazon_workflow, 
+                              new_data=amazonTest,
+                              type="prob") %>%
+  bind_cols(., amazonTest) %>%
+  select(id, .pred_1) %>%
+  rename(Action=.pred_1)
+
+
+# amazon_submit <- cbind(amazonTest, amazon_predictions) %>%
+#   select(RESOURCE, .pred_1)
+# colnames(amazon_submit) <- c("id", "Action")
+  
+vroom_write(x=amazon_predictions, file="./AmazonPreds.csv", delim=",")
+
+# create ID and predicted prob columns 
+# sample submission looks like ID and action
+
+
+
